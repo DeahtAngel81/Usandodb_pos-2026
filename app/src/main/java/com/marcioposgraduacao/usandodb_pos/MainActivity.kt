eb3b1be444044a204fbe63b24f64fb6b422cfec0
Marcio R. Silva <mr3475995@gmail.com>
2026-06-26 16:07:05 -0300

    Desenvolvido a tela do  MainActivity.kt incluindo a inclusã, alteração e pesquisa de daos do banco CloudFirestore

--- app/src/main/java/com/marcioposgraduacao/usandodb_pos/MainActivity.kt
+++ app/src/main/java/com/marcioposgraduacao/usandodb_pos/MainActivity.kt
@@ -3,14 +3,18 @@ package com.marcioposgraduacao.usandodb_pos
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log.e
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import com.marcioposgraduacao.usandodb_pos.database.DatabaseHandler
 import com.marcioposgraduacao.usandodb_pos.databinding.ActivityMainBinding
 import com.marcioposgraduacao.usandodb_pos.entity.Cadastro
@@ -18,6 +22,8 @@ import com.marcioposgraduacao.usandodb_pos.entity.Cadastro
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var banco: DatabaseHandler
+
+    val db = Firebase.firestore
 
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,7 +65,34 @@ class MainActivity : AppCompatActivity() {
 
     private fun alterar() {
 
-        val id = binding.etCod.text.toString().toIntOrNull()
+        val cadastro: HashMap<String, String> = hashMapOf(
+            "nome" to binding.etNome.text.toString(),
+            "telefone" to binding.etTelefone.text.toString(),
+
+        )
+
+        db
+            .collection("Cadastramento")
+            .document(binding.etCod.text.toString())
+            .set(cadastro)
+            .addOnSuccessListener {
+                makeText(
+                    this, "Operação efetuada com sucesso!",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                finish()
+
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this,
+                    "Erro ${e.message}", Toast.LENGTH_LONG).show()
+            }
+
+
+
+
+        /*val id = binding.etCod.text.toString().toIntOrNull()
 
         if (id == null) {
             val cadastro = Cadastro(
@@ -76,19 +109,35 @@ class MainActivity : AppCompatActivity() {
                 binding.etTelefone.text.toString()
             )
             banco.alterar(cadastro)
-        }
+        }*/
 
-        Toast.makeText(
-            this, "Operação efetuada com sucesso!",
-            Toast.LENGTH_LONG
-        ).show()
 
-        finish()
     }
 
     private fun excluir() {
 
-        val id = binding.etCod.text.toString().toIntOrNull()
+        db
+            .collection("Cadastramento")
+            .document(binding.etCod.text.toString())
+            .delete()
+            .addOnSuccessListener {
+
+                makeText(
+                    this, "Operação efetuada com sucesso!",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                finish()
+
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this,
+                    "Erro ${e.message}", Toast.LENGTH_LONG).show()
+
+            }
+
+
+        /*val id = binding.etCod.text.toString().toIntOrNull()
 
         if (id == null) {
             binding.etCod.error = "Digite Código válido"
@@ -97,11 +146,11 @@ class MainActivity : AppCompatActivity() {
 
         banco.excluir(id)
 
-        Toast.makeText(
+        makeText(
             this,
             "Exclusão efetuada com sucesso!",
             Toast.LENGTH_LONG
-        ).show()
+        ).show()*/
 
         finish()
     }
@@ -118,14 +167,31 @@ class MainActivity : AppCompatActivity() {
             val id: Int? = etCodPesquisa.text.toString().toIntOrNull()
 
             if (id == null) {
-                Toast.makeText(
+                makeText(
                     this, "Código deve ser informado...",
                     Toast.LENGTH_LONG
                 ).show()
 
             } else {
 
-                val cadastro = banco.pesquisar(id)
+                db
+                    .collection("Cadastramento")
+                    .document(id.toString())
+                    .get()
+                    .addOnSuccessListener { result ->
+                        binding.etCod.setText(etCodPesquisa.text.toString())
+                        binding.etNome.setText(result.get("nome").toString())
+                        binding.etTelefone.setText(result.get("telefone").toString())
+                    }
+                    .addOnFailureListener { e ->
+                        makeText(
+                            this,
+                            "Registro não encontrado!",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                    }
+               /* val cadastro = banco.pesquisar(id)
 
                 if (cadastro != null) {
                     binding.etCod.setText(etCodPesquisa.text.toString())
@@ -133,13 +199,14 @@ class MainActivity : AppCompatActivity() {
                     binding.etTelefone.setText(cadastro.telefone)
                 } else {
 
-                    Toast.makeText(
+                    makeText(
                         this,
                         "Registro não encontrado!",
                         Toast.LENGTH_LONG
-                    ).show()
-                }
+                    ).show()*/
+
             }
+
         }
         )
 
